19bf145ad0a5856312311ef53dc758e236715813
n10032711 <n10032711@VDI-VL04-001>
2019-02-27 12:45:58 +1000

    Still on Woirk

---
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.writeline("this is line 1");
+            //console.writeline("this is line 2");
+            //console.writeline("this is line 3");
+            //console.writeline("this is line 4");
+            //console.writeline("this is line 5");
+            for(int i = 0; i <=5; i++)
+            {
+                Console.WriteLine("this is line {0}", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
